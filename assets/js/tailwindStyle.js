de5dafd83e2dc44ea9f62c75c891286c7f693d9f
JoseRDlr <71417368+JoseRDlr@users.noreply.github.com>
2025-05-28 16:03:16 -0500

    ajustes

---
+++ assets/js/tailwindStyle.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+    theme: {
+      container: {
+        center: true,
+        padding: "2rem",
+        screens: {
+          "2xl": "1400px",
+        },
+      },
+      extend: {
+        colors: {
+          primary: '#25D366',
+          secondary: '#121E17',
+          accent: '#FF6B6B',
+        },
+        fontFamily: {
+          sans: ['Inter', 'sans-serif'],
+        },
+      }
+    }
+  }
